99b6d31b9ca48901c4e787e8257e68f5ec40cb6d
illia <rnrlla1994@gmail.com>
2024-12-10 12:57:12 +0100

    added host manager class. fixed host usages

--- src/test/java/base/BaseUiTest.java
+++ src/test/java/base/BaseUiTest.java
@@ -1,6 +1,7 @@
 package base;
 
 import api.config.Config;
+import api.spec.HostManager;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.testng.annotations.AfterMethod;
@@ -9,26 +10,40 @@ import org.testng.annotations.BeforeSuite;
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 @SuppressWarnings("all")
 public class BaseUiTest extends BaseTest {
-
+    /*Update 'environment' in config.properties before running tests*/
     @BeforeSuite(alwaysRun = true)
     protected void setupUiTest() {
-        Configuration.timeout = 20000;
-        Configuration.browser = Config.getProperty("browserFirefox");
-        Configuration.baseUrl = "http://" + Config.getProperty("host");
-        Configuration.remote = Config.getProperty("remote");
-//        Configuration.browserSize = Config.getProperty("browserSize");
-        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of(
-                "enableVNC", true,
-                "enableLog", true
-        ));
-        open();
+        if (HostManager.isLocal()) {
+            setupLocal();
+        } else setupRemote();
     }
 
     @AfterMethod(alwaysRun = true)
     protected void tearDownUiTest() {
         Selenide.closeWebDriver();
+    }
+
+    private void setupLocal() {
+        Configuration.timeout = 20000;
+        Configuration.browser = Config.getProperty("browserFirefox");
+        Configuration.baseUrl = "http://" + HostManager.getHost();
+        open();
+        getWebDriver().manage().window().maximize();
+    }
+
+    private void setupRemote() {
+        Configuration.timeout = 20000;
+        Configuration.browser = Config.getProperty("browserFirefox");
+        Configuration.baseUrl = "http://" + HostManager.getHost();
+        Configuration.remote = Config.getProperty("selenoidRemote");
+//        Configuration.browserSize = Config.getProperty("browserSize");
+        Configuration.browserCapabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableLog", true
+        ));
     }
 }
