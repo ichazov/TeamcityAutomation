5000386ad07dc209a705fcef3eda696e7ea5d89a
illia <rnrlla1994@gmail.com>
2026-02-23 14:37:47 +0100

    Deprecate Interactable and inline element interaction waits

--- src/main/java/ui/pages/build/NewBuildStepCommandLinePage.java
+++ src/main/java/ui/pages/build/NewBuildStepCommandLinePage.java
@@ -1,8 +1,11 @@
 package ui.pages.build;
 
 import api.generators.RandomData;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
 
 public class NewBuildStepCommandLinePage extends BaseBuildPage {
     private static final By NEW_RUNNER_ID_FIELD = By.id("newRunnerId");
@@ -14,7 +17,7 @@ public class NewBuildStepCommandLinePage extends BaseBuildPage {
 
     public void setupBuildStep(String script) {
         enterText(NEW_RUNNER_ID_FIELD, RandomData.getString());
-        getInteractableElement(ROOT, By.className("CodeMirror"));
+        $(ROOT).$(By.className("CodeMirror")).shouldBe(Condition.interactable);
         Selenide.executeJavaScript(
                 "document.querySelector('.CodeMirror').CodeMirror.setValue(arguments[0])", script
         );
